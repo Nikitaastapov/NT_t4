286ea46e158936d2da92839c9e13109391c3fce5
Nikitaastapov <nikitaastapov@gmail.com>
2022-02-16 16:20:02 +0300

    add a shopping list dict

--- main.py
+++ main.py
@@ -21,4 +21,29 @@ with open (full_name, 'r', encoding='UTF-8') as f:
             temp_dic['measure']=line[2]
             cook_book[meal].append(temp_dic)
         num += 1      
-print(cook_book)
+# print(cook_book)
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shopping_list = {}
+    for asked_meal in dishes:
+        if asked_meal not in cook_book.keys():
+            print(f'{asked_meal} нет в меню')
+        else:
+            add_to_buy(asked_meal, person_count, shopping_list)
+            # print('Принято')
+    return shopping_list
+
+def add_to_buy(asked_meal, person_count, shopping_list):
+    for ing in cook_book[asked_meal]:
+        if ing['ingredient_name'] not in shopping_list.keys():
+            # shopping_list[ing['ingredient_name']] = {}
+            shopping_list[ing['ingredient_name']]={'measure': ing['measure']}
+            shopping_list[ing['ingredient_name']]['quantity'] = int(ing['quantity'])*person_count
+        else:
+            shopping_list[ing['ingredient_name']]['quantity'] += int(ing['quantity'])*person_count
+  
+
+# print(get_shop_list_by_dishes(['Омлет','Фахитос'] , 2))
+# print(get_shop_list_by_dishes(['Омлет','Омлет'] , 1))
+# print(get_shop_list_by_dishes(['Рыба','Омлет'] , 1))
+# print(get_shop_list_by_dishes(['Омлет'] , 2))
